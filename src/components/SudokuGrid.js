ef523e6b953f486eb5c42471cd6d472187d1d47d
Rujengelal <rgelal13@gmail.com>
2021-04-18 21:03:05 +0545

    finally complete

--- src/components/SudokuGrid.js
+++ src/components/SudokuGrid.js
@@ -1,8 +1,29 @@
 import React, { useState } from "react";
 
+const initArray = () => {
+  let grid = [];
+  for (let i = 0; i < 9; i++) {
+    grid.push(Array(9).fill(0));
+  }
+  return grid;
+};
+
 function SudokuGrid(props) {
-  console.log(props.grid);
-  const [sudokuGrid, setSudokuGrid] = useState(props.grid);
+  // console.log(props.grid);
+  const [sudokuGrid, setSudokuGrid] = useState(initArray());
+  const [Status, setStatus] = useState(false);
+  const [StartSolving, setStartSolving] = useState(false);
+  // const [sudokuGrid, setSudokuGrid] = useState([
+  //   [3, 0, 6, 5, 0, 8, 4, 0, 0],
+  //   [5, 2, 0, 0, 0, 0, 0, 0, 0],
+  //   [0, 8, 7, 0, 0, 0, 0, 3, 1],
+  //   [0, 0, 3, 0, 1, 0, 0, 8, 0],
+  //   [9, 0, 0, 8, 6, 3, 0, 0, 5],
+  //   [0, 5, 0, 0, 9, 0, 6, 0, 0],
+  //   [1, 3, 0, 0, 0, 0, 2, 5, 0],
+  //   [0, 0, 0, 0, 0, 0, 0, 7, 4],
+  //   [0, 0, 5, 2, 0, 6, 3, 0, 0],
+  // ]);
 
   const isValid = (grid, value, i, j) => {
     for (let checkI = 0; checkI < 9; checkI++) {
@@ -20,34 +41,61 @@ function SudokuGrid(props) {
     }
     return true;
   };
+  let grid;
   const solve = () => {
     for (let i = 0; i < 9; i++) {
       for (let j = 0; j < 9; j++) {
+        // console.log("checking for 0 at", i, j);
         if (sudokuGrid[i][j] == 0) {
           console.log("0 at ", i, j);
           for (let k = 1; k <= 9; k++) {
-            console.log(sudokuGrid);
-            if (isValid(sudokuGrid, k, i, j)) {
-              const grid = sudokuGrid;
+            // console.log(sudokuGrid);
+            let goodValue = isValid(sudokuGrid, k, i, j);
+            // console.log(
+            //   `trying to solve at i=${i} and j=${j} and k=${k}`,
+            //   sudokuGrid[i]
+            // );
+            // console.log(`trying to solve at i=${i} and j=${j} and k=${k}`);
+            if (goodValue) {
+              grid = sudokuGrid;
               grid[i][j] = k;
               setSudokuGrid(grid);
-              solve();
-              grid[i][j] = 0;
-              setSudokuGrid(grid);
+              console.log("Setting value", k);
+              let nextStep = solve();
+              if (!nextStep) {
+                console.log("resetting value", k);
+
+                grid[i][j] = 0;
+                setSudokuGrid(grid);
+              } else {
+                return true; //TODO: Seeif it works with this
+              }
             }
           }
+          return false;
         }
       }
     }
+    return true;
   };
   return (
-    <>
+    <div
+      style={{
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
       <div>
         {sudokuGrid.map((val, i) => (
           <div
+            // key={`${i}`}
             style={{
               display: "grid",
-              gridTemplateColumns: "repeat(9, 40px)",
+              gridTemplateColumns: "repeat(9,1fr)",
             }}
           >
             {val.map((value, j) => {
@@ -61,6 +109,8 @@ function SudokuGrid(props) {
                     alignItems: "center",
                     height: "40px",
                     width: "40px",
+                    borderRightWidth: (j + 1) % 3 == 0 ? "5px" : "1px",
+                    borderBottomWidth: (i + 1) % 3 == 0 ? "5px" : "1px",
                   }}
                 >
                   <input
@@ -72,9 +122,11 @@ function SudokuGrid(props) {
                     }}
                     onChange={(e) => {
                       let newGrid = [...sudokuGrid];
-                      newGrid[i][j] = parseInt(e.target.value);
+                      newGrid[i][j] = parseInt(
+                        !e.target.value ? "0" : e.target.value
+                      );
                       setSudokuGrid(newGrid);
-                      console.log(newGrid, e.target.value);
+                      // console.log(newGrid, e.target.value);
                     }}
                   ></input>
                 </div>
@@ -83,22 +135,51 @@ function SudokuGrid(props) {
           </div>
         ))}
       </div>
-      <div>
-        <button
-          style={{
-            color: "black",
-            background: "#eee",
-            padding: "5px",
-          }}
-          onClick={() => {
-            solve();
-            // console.log(isValid(sudokuGrid, 7, 0, 0));
-          }}
-        >
-          Solve
-        </button>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <div>
+          <button
+            style={{
+              color: "black",
+              background: "#eee",
+              padding: "5px",
+            }}
+            onClick={() => {
+              setStatus(false);
+              setStartSolving(true);
+              if (!solve()) {
+                console.log("cannot be solved");
+              }
+              console.log("Done");
+              setStatus(true);
+              // console.log(isValid(sudokuGrid, 7, 0, 0));
+            }}
+          >
+            Solve
+          </button>
+          <button
+            style={{
+              color: "black",
+              background: "#eee",
+              padding: "5px",
+            }}
+            onClick={() => {
+              setSudokuGrid(initArray());
+              setStartSolving(false);
+              setStatus(false);
+            }}
+          >
+            Reset
+          </button>
+        </div>
+        <div>{StartSolving ? (Status ? "Solved" : "Solving") : ""}</div>
       </div>
-    </>
+    </div>
   );
 }
 export default SudokuGrid;
